35cf6057ff209e3fd0b224e0e6f2ee8b631a424f
srujanaremalli <101311015+srujanaremalli@users.noreply.github.com>
2022-03-10 11:08:11 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#add
+def add(x,y):
+	return x+y
+#sub
+def sub(x,y):
+	return x-y
+#div
+def div(x,y):
+
+	if y==0:
+		return error
+	else:
+		return x/y
+
+	
+#mul
+def mul(x,y):
+	return x*y
+#sqaure
+def square(x):
+	return x*x
